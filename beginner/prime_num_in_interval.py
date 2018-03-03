a94497c1ed9a03ede3e4cbdc105ac72bfc9819d2
preethikashree <36124177+preethikashree@users.noreply.github.com>
2018-03-03 18:57:10 +0530

    Create prime_num_in_interval.py

---
+++ beginner/prime_num_in_interval.py
@@ -0,0 +1,10 @@
+m=int(input("Enter two numbers:"))
+n=int(input())
+
+for num in range(m,n+1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
