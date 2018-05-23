76e537cc589d99defd6b9724e83ea7dc8e532386
preethikashree <36124177+preethikashree@users.noreply.github.com>
2018-05-23 22:17:06 +0530

    Update prime_or_not.py

--- beginner/prime_or_not.py
+++ beginner/prime_or_not.py
@@ -1,9 +1,9 @@
-num=int(input("Enter a number:"))
+num=int(input())
 for i in range(2,1001):
     if (num%i)!=0 and num%1==0:
-        print("Prime number")
+        print("yes")
         break
     else:
-        print("Not a prime number")
+        print("no")
         break
         
